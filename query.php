a9d815584ab1dc5e6fd430dc8be2c8a131e9ba65
Yuri Fontella <yurifc4@gmail.com>
2017-12-06 19:36:28 -0200

    first attack

--- query.php
+++ query.php
@@ -9,7 +9,7 @@ class Query
     $item = $_POST['item'];
 
     $connection =  Connection::getInstance()->getConnection();
-    $result = $connection->query("SELECT item, description, value FROM products WHERE item LIKE '%$item'");
+    $result = $connection->query("SELECT item, description, value FROM products WHERE item LIKE '%$item%'");
 
     return $result;
   }
